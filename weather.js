3507ffe2555f42931e3875157b451e0483953acd
Madhav Bist <64509612+bistmadhav@users.noreply.github.com>
2020-10-06 23:33:27 -0500

    API_CALL

--- weather.js
+++ weather.js
@@ -1,6 +1,12 @@
 
-let API_DATA= "a8e71c9932b20c4ceb0ceb0ad183e6a83bb";
+let API_data = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
 weather_data = (city)=> {
     const URL = "https://api.openweathermap.org/data/2.5/weather";
-    const URL_REST = `${URL}?q=${city}&applied=${API_DATA}&units=imperial`
+    const URL_complete = `${URL}?q=${city}&appid=${API_data}&units=imperial`;
+    const promise_data = fetch(URL_complete);
+    return promise_data.then((response)=>{
+        return response.json();
+    })
 }
+console.log(weather_data("arlington"));
